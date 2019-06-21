fc1f46e8753adef40b35b5c6635f545a443780a3
Fathima Sajitha <fathi.saji8@gmail.com>
2019-06-21 15:48:26 +0530

    First Release For QA

--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/project/controller/ProjectController.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/project/controller/ProjectController.java
@@ -1,13 +1,13 @@
 package com.sgic.internal.defecttracker.project.controller;
 
-import java.sql.Date;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,23 +20,21 @@ import com.sgic.common.api.enums.RestApiResponseStatus;
 import com.sgic.common.api.response.ApiResponse;
 import com.sgic.internal.defecttracker.project.controller.dto.ProjectData;
 import com.sgic.internal.defecttracker.project.controller.dtomapper.ProjectDtoMapper;
-
-import com.sgic.internal.defecttracker.project.services.ProjectService;
+import com.sgic.internal.defecttracker.project.repositories.ProjectRepository;
 import com.sgic.internal.defecttracker.project.services.impl.ProjectServiceImpl;
 
+@CrossOrigin
 @RestController
 public class ProjectController {
 	
 
-	private static Logger logger = LoggerFactory.getLogger(ProjectServiceImpl.class);
+	private static Logger logger =LogManager.getLogger(ProjectRepository.class);
 	
 	@Autowired
 	public ProjectDtoMapper projectDtoMapper;
-	
-//	@PostMapping(value="/project")
-//	public ResponseEntity<Object>createProject(@RequestBody   )
 
-	// Post Mapping For Create a Project by sajitha
+	//Author :: by Sajitha
+	//Post Mapping For Create a Project 
 	@PostMapping(value = "/createproject")
 	public ResponseEntity<Object> createProject(@RequestBody ProjectData projectData) {
 		projectDtoMapper.saveProjectforMapper(projectData);
@@ -44,42 +42,49 @@ public class ProjectController {
 		return new ResponseEntity<>(new ApiResponse(RestApiResponseStatus.OK), HttpStatus.OK);
 	}
 
-	// Get Mapping For Get All Project by Mercy
+	//Author :: by Mercy
+	//Post Mapping For Create a Project 
 	@GetMapping(value = "/GetAllproject")
 	public ResponseEntity<List<ProjectData>> listEmployeeInfo() {
 		logger.info("Project are listed ");
 		return new ResponseEntity<>(projectDtoMapper.getAllProjectForMapper(), HttpStatus.OK);
 	}
 
-	// Get Mapping For Get Project By Id Jakki
+	//Author :: by jakki
+	//Get Mapping For Get Project By Id
 	@GetMapping("/getProjectById/{id}")
 	public ResponseEntity<ProjectData> getProjectById(@PathVariable Long id) {
 		logger.info("Projects are get by id ");
 		return new ResponseEntity<>(projectDtoMapper.getByProjectId(id), HttpStatus.OK);
 	}
-
-	// Delete Mapping For Project By thadsha
+	
+	//Author ::  By thadsha
+	// Delete Mapping For Project
 	@DeleteMapping("deleteById/{projectId}")
 	public ResponseEntity<ProjectData> deleteById(@PathVariable Long projectId) {
 		logger.info("Projects are delete by id ");
 		return new ResponseEntity<>(projectDtoMapper.deleteById(projectId), HttpStatus.OK);
 	}
 
-	// Put Mapping For Project By Arany
+	//Author :: By Arany
+    //Put Mapping For Project
 	@PutMapping("/updateProject/{projectid}")
 	public ResponseEntity<String> updateProject(@PathVariable(name = "projectid") Long projectid,
-			@RequestBody ProjectData projectData) {
-		projectDtoMapper.UpdateProject(projectid, projectData);
-		return new ResponseEntity<>("user updated succesfully", HttpStatus.OK);
+	@RequestBody ProjectData projectData) {
+	logger.info ("Projectcontroller -> updatedproject");
+	if(projectDtoMapper.UpdateProject(projectid, projectData) !=null);
+	{
+	return new ResponseEntity<>("ok", HttpStatus.OK);
 	}
-
-	// Get Mapping For Project Name By Sujanthan
+	}
+	//Author :: By Sujanthan
+	// Get Mapping For Project Name
 	@GetMapping("/getName/{name}")
 	public List<ProjectData> getByprojectName(@PathVariable String name) {
 		logger.info("Projects are get by name ");
 		return projectDtoMapper.getByprojectNameForMapper(name);
 	}
-
+	//Author ::  By Sajitha
 	// Get Mapping For Project Type By Sajitha
 	@GetMapping("/gettype/{type}")
 	public List<ProjectData> getBytype(@PathVariable String type) {
@@ -87,7 +92,8 @@ public class ProjectController {
 		return projectDtoMapper.getByProjecttype(type);
 	}
 
-	// Get Mapping For Project Start Date By thatsha
+	//Author :: By thatsha
+	// Get Mapping For Project Start Date
 	@GetMapping("/getDate/{date}")
 	public List<ProjectData> getBystartDate(@PathVariable String date) {
 		logger.info("Projects are get by date ");
@@ -95,14 +101,16 @@ public class ProjectController {
 
 	}
 
-	// Get Mapping For Project duration By Aarany
+	//Author :: By Aarany
+	// Get Mapping For Project duration
 	@GetMapping("/getduration/{duration}")
 	public List<ProjectData> getByduration(@PathVariable String duration) {
 		logger.info("Projects are get by duration");
 		return projectDtoMapper.getBydurationformapper(duration);
 	}
 
-	// Get Mapping For Project Start Date By Mercy
+	//Author ::By Mercy
+	// Get Mapping For Project Status
 	@GetMapping("/getstatus/{status}")
 	public List<ProjectData> getBystatus(@PathVariable String status) {
 		logger.info("Projects are get by status");
